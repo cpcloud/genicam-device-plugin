ed619ed8e9de6426f49e3772efdd78037501818c
Brandon Ogle <brandon@standardcognition.com>
2020-07-31 13:45:21 -0700

    Setup envvars for development enivornment and link binaries in root

--- shell.nix
+++ shell.nix
@@ -2,4 +2,9 @@ with (import ./nixpkgs.nix);
 mkShell {
   buildInputs = [ plugin-launcher ];
   inputsFrom = [ genicam-device-plugin ];
+  shellHook = ''
+    export GOPATH=$(mktemp -d)
+    export GOBIN=$GOPATH/bin
+    ln -s $GOBIN bin
+  '';
 }
