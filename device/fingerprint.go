9ccfd3affc187ec7ca290ad3af30f04ac0c0575d
Brandon Ogle <brandon@standardcognition.com>
2020-07-31 13:45:49 -0700

    Stuff in some static values for the device fingerprint

--- device/fingerprint.go
+++ device/fingerprint.go
@@ -33,9 +33,11 @@ func (d *GenicamDevicePlugin) doFingerprint(ctx context.Context, devices chan *d
 //
 // plugin implementations will likely have a native struct provided by the corresonding SDK
 type fingerprintedDevice struct {
-	ID         string
-	deviceName string
-	PCIBusID   string
+    cam_num      int64
+    model        string
+    manufacturer string
+    serial       string
+    iface        string
 }
 
 // writeFingerprintToChannel collects fingerprint info, partitions devices into
@@ -55,37 +57,38 @@ func (d *GenicamDevicePlugin) writeFingerprintToChannel(devices chan<- *device.F
 		d.deviceLock.Lock()
 		defer d.deviceLock.Unlock()
 
-		// "discover" some devices
+		//// "discover" some devices
 		discoveredDevices := []*fingerprintedDevice{
 			{
-				ID:         uuid.Generate(),
-				deviceName: "modelA",
-				PCIBusID:   uuid.Generate(),
-			},
-			{
-				ID:         uuid.Generate(),
-				deviceName: "modelB",
-				PCIBusID:   uuid.Generate(),
+				cam_num:       0,
+				model:        "DFK 33GX264E",
+				manufacturer: "TIS",
+                serial:       "01810549",
+                iface:        "Gige",
 			},
 		}
 
-		// during fingerprinting, devices are grouped by "device group" in
-		// order to facilitate scheduling
-		// devices in the same device group should have the same
-		// Vendor, Type, and Name ("Model")
-		// Build Fingerprint response with computed groups and send it over the channel
-		deviceListByDeviceName := make(map[string][]*fingerprintedDevice)
-		for _, device := range discoveredDevices {
-			deviceName := device.deviceName
-			deviceListByDeviceName[deviceName] = append(deviceListByDeviceName[deviceName], device)
-			d.devices[device.ID] = deviceName
-		}
+		//// during fingerprinting, devices are grouped by "device group" in
+		//// order to facilitate scheduling
+		//// devices in the same device group should have the same
+		//// Vendor, Type, and Name ("Model")
+		//// Build Fingerprint response with computed groups and send it over the channel
+		//deviceListByDeviceName := make(map[string][]*fingerprintedDevice)
+		//for _, device := range discoveredDevices {
+        //    camNum = device.cam_num;
+		//	deviceName := device.deviceName
+		//	deviceListByDeviceName[cam_num] = append(deviceListByDeviceName[deviceName], device)
+		//	d.devices[cam_num] = deviceName
+		//}
 
-		// Build Fingerprint response with computed groups and send it over the channel
-		deviceGroups := make([]*device.DeviceGroup, 0, len(deviceListByDeviceName))
-		for groupName, devices := range deviceListByDeviceName {
-			deviceGroups = append(deviceGroups, deviceGroupFromFingerprintData(groupName, devices))
-		}
+		//// Build Fingerprint response with computed groups and send it over the channel
+		//deviceGroups := make([]*device.DeviceGroup, 0, len(deviceListByDeviceName))
+		//for groupName, devices := range deviceListByDeviceName {
+		//	deviceGroups = append(deviceGroups, deviceGroupFromFingerprintData(groupName, devices))
+		//}
+        deviceGroups := make([]*device.DeviceGroup, 0, 1)
+        deviceGroups = append(deviceGroups, deviceGroupFromFingerprintData("taco", discoveredDevices))
+
 		devices <- device.NewFingerprint(deviceGroups...)
 	}
 }
@@ -100,33 +103,34 @@ func deviceGroupFromFingerprintData(groupName string, deviceList []*fingerprinte
 	devices := make([]*device.Device, 0, len(deviceList))
 	for _, dev := range deviceList {
 		devices = append(devices, &device.Device{
-			ID:      dev.ID,
+			ID:      dev.serial,
 			Healthy: true,
 			HwLocality: &device.DeviceLocality{
-				PciBusID: dev.PCIBusID,
+				PciBusID: "GigE",
 			},
 		})
 	}
 
 	deviceGroup := &device.DeviceGroup{
-		Vendor:  vendor,
-		Type:    deviceType,
-		Name:    groupName,
+		Vendor: "TIS",
+		Type:   "Camera",
+        Name:   "DFK 33GX264E",
 		Devices: devices,
 		// The device API assumes that devices with the same DeviceName have the same
 		// attributes like amount of memory, power, bar1memory, etc.
 		// If not, then they'll need to be split into different device groups
 		// with different names.
-		Attributes: map[string]*structs.Attribute{
-			"attrA": {
-				Int:  helper.Int64ToPtr(1024),
-				Unit: "MB",
-			},
-			"attrB": {
-				Float: helper.Float64ToPtr(10.5),
-				Unit:  "MW",
-			},
-		},
+		Attributes: map[string]*structs.Attribute{},
+        //{
+		//	"attrA": {
+		//		Int:  helper.Int64ToPtr(1024),
+		//		Unit: "MB",
+		//	},
+		//	"attrB": {
+		//		Float: helper.Float64ToPtr(10.5),
+		//		Unit:  "MW",
+		//	},
+		//},
 	}
 	return deviceGroup
 }
